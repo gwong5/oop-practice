2e527b1b9f7e69037dfc7c4da001d423bb07ebce
greg <kousagi1012@yahoo.com>
2017-06-05 13:28:41 -0700

    remove all references to Frame
    Frame is not being used in stage 3.

--- bike-shop/src/stage3-methods.js
+++ bike-shop/src/stage3-methods.js
@@ -2,16 +2,11 @@ function Bike() {
   // your code here
 }
 
-function Frame() {
-  // your code here
-}
-
 function Tire() {
   // your code here
 }
 
 module.exports = {
   Bike: Bike,
-  Frame: Frame,
   Tire: Tire
 }
